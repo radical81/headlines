78a03b64b0d7db41e0a1001dee69f42519b87f3d
Rex Jason <ralobba@gmail.com>
2023-12-06 15:20:40 +1100

    Setup Tabs' view controllers

---
+++ ArticlesHeadlines/Controller/HeadlinesViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HeadlinesViewController.swift
+//  ArticlesHeadlines
+//
+//  Created by Rex Jason Alobba on 6/12/2023.
+//
+
+import UIKit
+
+class HeadlinesViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
