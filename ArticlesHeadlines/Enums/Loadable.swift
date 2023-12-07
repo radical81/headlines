b1c3576eb0825da2c5b46124d15875882ba14a02
Rex Jason <ralobba@gmail.com>
2023-12-08 10:52:39 +1100

    API: Setup Loadable enum and news retriever protocol.

---
+++ ArticlesHeadlines/Enums/Loadable.swift
@@ -0,0 +1,26 @@
+//
+//  Loadable.swift
+//  ArticlesHeadlines
+//
+//  Created by Rex Jason Alobba on 8/12/2023.
+//
+
+import Foundation
+
+/// Loadable tracks the state a request is in when it is sent from the app to the API.
+enum Loadable<T> {
+  case notLoaded
+  case loading
+  case loaded(T)
+  case failed(Error)
+  
+  /// The value of the loaded data
+  var value: T? {
+    switch self {
+    case .loaded(let t):
+      return t
+    default:
+      return nil
+    }
+  }
+}
---
+++ ArticlesHeadlines/Protocols/NewsRetriever.swift
@@ -0,0 +1,16 @@
+//
+//  NewsRetriever.swift
+//  ArticlesHeadlines
+//
+//  Created by Rex Jason Alobba on 8/12/2023.
+//
+
+import Foundation
+
+/// Hides the implementation of API calls to the news service.
+protocol NewsRetriever {
+  /// Fetches the headlines.
+  func fetchHeadlines(_ sources: [Source]) async -> Loadable<[Headline]>
+  /// Retrieve news sources.
+  func fetchSources() async -> Loadable<[Source]>
+}
