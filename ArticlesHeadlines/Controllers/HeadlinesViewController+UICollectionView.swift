1217bd0ee98b500ab6c75ee323987ed7965b16f5
Rex Jason <ralobba@gmail.com>
2023-12-07 12:41:22 +1100

    Collection view: Prepare to select item

--- ArticlesHeadlines/Controllers/HeadlinesViewController+UICollectionView.swift
+++ ArticlesHeadlines/Controllers/HeadlinesViewController+UICollectionView.swift
@@ -10,15 +10,19 @@ import UIKit
 
 /// Implementation of UICollectionView delegate and data source.
 extension HeadlinesViewController: UICollectionViewDataSource, UICollectionViewDelegate {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-      headlines.count
+  func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+    headlines.count
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+    guard let headlineItem = collectionView.dequeueReusableCell(withReuseIdentifier: "HeadlineList", for: indexPath) as? HeadlineItemCollectionViewCell else {
+      return UICollectionViewCell()
     }
+    headlineItem.headline = headlines[indexPath.row]
+    return headlineItem
+  }
+  
+  func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
     
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-      guard let headlineItem = collectionView.dequeueReusableCell(withReuseIdentifier: "HeadlineList", for: indexPath) as? HeadlineItemCollectionViewCell else {
-        return UICollectionViewCell()
-      }
-      headlineItem.headline = headlines[indexPath.row]
-      return headlineItem
-    }
+  }
 }
