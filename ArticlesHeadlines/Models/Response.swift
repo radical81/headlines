a1a86613be34c703851bac1165b9d967aca9c8a3
Rex Jason <ralobba@gmail.com>
2023-12-08 12:30:37 +1100

    Fetch data, track response as its own model.

---
+++ ArticlesHeadlines/Models/Response.swift
@@ -0,0 +1,26 @@
+//
+//  Response.swift
+//  ArticlesHeadlines
+//
+//  Created by Rex Jason Alobba on 8/12/2023.
+//
+
+import Foundation
+
+/// Model to capture the responses from the API.
+struct Response: Decodable {
+  /// News articles
+  var articles: [Headline]
+//  var sources: [Source]
+  
+  /// Use to decode from JSON.
+  init(from decoder: Decoder) throws {
+    let values = try decoder.container(keyedBy: CodingKeys.self)
+    articles = try values.decode([Headline].self, forKey: .articles)
+//    sources = try values.decode([Source].self, forKey: .sources)
+  }
+  
+  enum CodingKeys: String, CodingKey {
+    case articles //, sources
+  }
+}
--- ArticlesHeadlines/Services/NewsApi.swift
+++ ArticlesHeadlines/Services/NewsApi.swift
@@ -17,68 +17,50 @@ struct NewsApi: NewsRetriever {
     
   /// News API base URL.
   var baseUrl: String {
-    "https://newsapi.org"
+    "https://newsapi.org/v2"
   }
   
   /// Fetches the headlines.
   func fetchHeadlines(_ sources: [Source]) async -> Loadable<[Headline]> {
-//    guard var components = URLComponents(string: baseUrl) else {
-//      return .failed(APIError.malformed("Invalid URL."))
-//    }
-//
-//    let sources = LocalStore().newsSources.map{ $0.id }.joined(separator: ",")
-//    components.queryItems = [
-//        URLQueryItem(name: "sources", value: sources),
-//        URLQueryItem(name: "")
-//    ]
-//
-//    guard let url = components.url else {
-//        throw URLError(.badURL)
-//    }
-//    
-//    guard let url = URL(String: baseUrl) else {
-//      return .failed(APIError.malformed("Invalid URL."))
-//    }
-//    var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
-//    request.httpMethod = "GET"
-//    var encodedData: Data?
-//    do {
-//      encodedData = try JSONEncoder().encode(person)
-//    } catch {
-//      return .failed(APIError.missingRequestDetails("Name or email must be missing in the request payload."))
-//    }
-//    guard let encodedData = encodedData else {
-//      return .failed(APIError.missingRequestDetails("Name or email must be missing in the request payload."))
-//    }
-//    request.httpBody = encodedData
-//    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
-//    let decoder = JSONDecoder()
-//    var dataResponse: (Data, URLResponse)?
-//    do {
-//      dataResponse = try await URLSession.shared.data(for: request)
-//    } catch {
-//      return .failed(APIError.network("Network error or invalid endpoint."))
-//    }
-//    guard let response = dataResponse?.1 as? HTTPURLResponse else {
-//      return .failed(APIError.network("No response from the API."))
-//    }
-//    guard let data = dataResponse?.0 else {
-//      return .failed(APIError.network("Missing data from the API."))
-//    }
-//    if response.statusCode == 400 { // Bad request
-//      guard let responseDetails = try? decoder.decode(Response.self, from: data), let errorMessage = responseDetails.errorMessage else {
-//        return .failed(APIError.badRequest("Bad request, the service cannot handle this."))
-//      }
-//      return .failed(APIError.badRequest(errorMessage))
-//    }
-//    guard response.statusCode == 200, let stringResponse = String(data: data, encoding: .utf8), stringResponse == "Registered" else {
-//      return .failed(APIError.registrationFail)
-//    }
-//    return .loaded(Response(registered: true))
+    guard var components = URLComponents(string: baseUrl + "/top-headlines") else {
+      return .failed(APIError.malformed("Invalid URL."))
+    }
+
+    let sources = LocalStore().newsSources.map{ $0.id }.joined(separator: ",")
+    components.queryItems = [
+        URLQueryItem(name: "sources", value: sources),
+        URLQueryItem(name: "apiKey", value: apiKey)
+    ]
+
+    guard let url = components.url else {
+      return .failed(APIError.malformed("Invalid URL."))
+    }
     
-    
-    return .notLoaded
-    
+    var request = URLRequest(url: url, cachePolicy: .useProtocolCachePolicy, timeoutInterval: 10.0)
+    request.httpMethod = "GET"
+    request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+    let decoder = JSONDecoder()
+    var dataResponse: (Data, URLResponse)?
+    do {
+      dataResponse = try await URLSession.shared.data(for: request)
+    } catch {
+      return .failed(APIError.network("Network error or invalid endpoint."))
+    }
+    guard let response = dataResponse?.1 as? HTTPURLResponse else {
+      return .failed(APIError.network("No response from the API."))
+    }
+    guard let data = dataResponse?.0 else {
+      return .failed(APIError.network("Missing data from the API."))
+    }
+    if response.statusCode == 400 { // Bad request
+      return .failed(APIError.badRequest("Bad request, the service cannot handle this."))
+    }
+    guard response.statusCode == 200,
+          let response = try? decoder.decode(Response.self, from: data)
+    else {
+      return .failed(APIError.unknown)
+    }
+    return .loaded(response.articles)
   }
   
   /// Retrieve news sources.
