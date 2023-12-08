8f6de45d88f634ed59a58a3d155cb1ed0a30ee0e
Rex Jason <ralobba@gmail.com>
2023-12-08 12:31:29 +1100

    Fetch real news data.

--- ArticlesHeadlines/Controllers/HeadlinesViewController.swift
+++ ArticlesHeadlines/Controllers/HeadlinesViewController.swift
@@ -9,13 +9,33 @@ import UIKit
 
 class HeadlinesViewController: UIViewController {
   
+//  /// The API service retrieve the headlines.
+//  let api: NewsRetriever
 
   /// The list of headlines, a collection view.
   var headlinesList: HeadlinesList?
   
   /// The data source for the list.
-  var headlines: [HeadlineViewModel] = []
+  var dataForLoading: Loadable<[Headline]> = .notLoaded {
+    didSet {
+      switch dataForLoading {
+      case .loaded(let data):
+        headlines = data.map {
+          HeadlineViewModel($0)
+        }
+      default:
+        break
+      }
+    }
+  }
   
+  /// The view model instances for the list.
+  var headlines: [HeadlineViewModel] = [] {
+    didSet {
+      headlinesList?.reloadData()
+    }
+  }
+
   override func viewDidLoad() {
     super.viewDidLoad()
     headlinesList = HeadlinesList(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
@@ -23,17 +43,13 @@ class HeadlinesViewController: UIViewController {
     headlinesList?.dataSource = self
     headlinesList?.delegate = self
     view.addSubview(headlinesList ?? UICollectionView())
-    fetchHeadlines()
+    Task {
+      await fetchHeadlines()
+    }
   }
   
   /// Fetch data/
-  func fetchHeadlines() {
-    //TODO: Real data
-    headlines = [
-      HeadlineViewModel(Headline(title: "test", description: "testing lots of lots of text the quick brown fox jumps over the lazy dog near the river bank", author: "rex", url: "https://www.news-medical.net/news/20231204/Brown-seaweed-emerges-as-a-potent-ally-in-diabetes-control.aspx", urlToImage: "https://d2jx2rerrg6sh3.cloudfront.net/images/news/ImageForNews_766206_17017494326985532.jpg")),
-      HeadlineViewModel(Headline(title: "test 2", description: "testing again", author: "rex", url: "", urlToImage: "")),
-      HeadlineViewModel(Headline(title: "test 3", description: "testing", author: "rex", url:"", urlToImage: "https://d2jx2rerrg6sh3.cloudfront.net/images/news/ImageForNews_766206_17017494326985532.jpg")),
-      HeadlineViewModel(Headline(title: "test 4", description: "testing again", author: "rex", url: "", urlToImage: "")),
-    ]
+  func fetchHeadlines() async {
+    dataForLoading = await NewsApi().fetchHeadlines([])
   }
 }
