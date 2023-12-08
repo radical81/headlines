0f600a9903092ffc9f58e986a5e3c2a9b641d0ad
Rex Jason <ralobba@gmail.com>
2023-12-08 15:10:41 +1100

    Amend doco

--- ArticlesHeadlines/Services/Shared.swift
+++ ArticlesHeadlines/Services/Shared.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-/// Keeps track of the instance of the API.
+/// Keeps track of a singleton that stores services.
 struct Shared {
   /// Shared is an instance of the news retriever.
   /// It is of type NewsRetriever and the implementation instance is NewsAPI(). This can be changed later on to point to a different API.
