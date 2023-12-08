491fd268ff78a9de6cfae4105aa6cce33239b423
Rex Jason <ralobba@gmail.com>
2023-12-08 12:55:14 +1100

    Cleanup

--- ArticlesHeadlines/Controllers/ArticleViewController.swift
+++ ArticlesHeadlines/Controllers/ArticleViewController.swift
@@ -70,15 +70,4 @@ class ArticleViewController: UIViewController, WKUIDelegate {
     }
     self.present(confirmation, animated: true)
   }
-  
-  /*
-   // MARK: - Navigation
-   
-   // In a storyboard-based application, you will often want to do a little preparation before navigation
-   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-   // Get the new view controller using segue.destination.
-   // Pass the selected object to the new view controller.
-   }
-   */
-
 }
