61baa23a8be47ad2684ad428efd6caa23371bf64
Rex Jason <ralobba@gmail.com>
2024-05-23 00:31:01 +1000

    Add assertion for second headline for unit test.

--- ArticlesHeadlinesTests/ViewModelTests/HeadlinesViewModelTests.swift
+++ ArticlesHeadlinesTests/ViewModelTests/HeadlinesViewModelTests.swift
@@ -15,8 +15,11 @@ final class HeadlinesViewModelTests: XCTestCase {
     // Fetch data
     var expectedFirstHeadline = Headline()
     expectedFirstHeadline.title = "Accenture set to buy Partners in Performance"
+    var expectedSecondHeadline = Headline()
+    expectedSecondHeadline.title = "ASX sharemarket: UBS strategist Richard Schellbach says Wall Street’s relentless grind higher will flow through to a mini melt-up, and picks 12 local stocks to watch"
     await viewModel.fetchHeadlines()
     XCTAssertEqual(viewModel.dataForLoading, .loaded([]))
     XCTAssertEqual(viewModel.headlines.first, expectedFirstHeadline, "The headlines are different.")
+    XCTAssertEqual(viewModel.headlines[1], expectedSecondHeadline, "The headlines are different.")
   }
 }
